bb1f89944d5df3019a7bab18e19b188a34299c35
Dima Kogan <dima@secretsauce.net>
2013-04-19 01:29:32 -0700

    removed unneeded assertions. I check these at the perl level

--- template_complex.c
+++ template_complex.c
@@ -1,8 +1,4 @@
-// This is the template used by PP to generate the FFTW routines. This is passed
-// into pp_def() in the 'Code' key. Before this file is passed to pp_def, the
-// following strings are replaced:
-//
-// RANK            This is the rank of the FFT being generated
+// This is the template used by PP to generate the FFTW routines.
 
 #ifndef __TEMPLATE_ALREADY_INCLUDED__
 
@@ -16,14 +12,6 @@
 {
   // make sure the PDL data type I'm using matches the FFTW data type
   static_assert( sizeof($GENERIC())*2 == sizeof($TFD(fftwf_,fftw_)complex) );
-
-  // PP asked for these dimensions, so these should all be fine. If they aren't,
-  // PP messed up somehow
-  assert( $PDL(in) ->ndims == RANK+1 );
-  assert( $PDL(out)->ndims == RANK+1 );
-  assert( $PDL(in)->dims[0] == 2 );
-  for( int i=0; i<RANK; i++ )
-    assert( $PDL(in)->dims[i] == $PDL(out)->dims[i] );
 
   $TFD(fftwf_,fftw_)plan plan = INT2PTR( $TFD(fftwf_,fftw_)plan, $COMP(plan));
   $TFD(fftwf_,fftw_)execute_dft( plan,
